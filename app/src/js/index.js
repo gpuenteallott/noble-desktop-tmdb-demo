a9b11aef6bc872502eba34ac5c3d27cd92e563be
Guillermo de la Puente <g.puente.allott@gmail.com>
2016-11-18 11:19:21 -0500

    Uncommented code after demo

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -7,44 +7,42 @@ var $ = require('jquery');
   var tmdbBaseUrl = window.bootstrapData.tmdbBaseUrl;
   var posterBaseImageUrl = 'https://image.tmdb.org/t/p/w185';
 
-  // function getPopulatedMovieCardMarkup(movie) {
-  //   var posterPath = movie.poster_path;
-  //   var originalTitle = movie.original_title;
-  //   var releaseDate = movie.release_date;
+  function getPopulatedMovieCardMarkup(movie) {
+    var posterPath = movie.poster_path;
+    var originalTitle = movie.original_title;
+    var releaseDate = movie.release_date;
 
-  //   return (
-  //     '<div class="movie-card">' +
-  //       '<img class="movie-card-poster-image" src="' + posterBaseImageUrl + posterPath + '">' +
-  //       '<div class="movie-card-title">' + originalTitle + '</div>' +
-  //       '<div class="movie-card-release-year">' + releaseDate + '</div>' +
-  //     '</div>'
-  //   );
-  // }
+    return (
+      '<div class="movie-card">' +
+        '<img class="movie-card-poster-image" src="' + posterBaseImageUrl + posterPath + '">' +
+        '<div class="movie-card-title">' + originalTitle + '</div>' +
+        '<div class="movie-card-release-year">' + releaseDate + '</div>' +
+      '</div>'
+    );
+  }
 
-  // // Document ready.
-  // $(function() {
-  //   $.ajax({
-  //     url: tmdbBaseUrl + 'movie/popular',
-  //     data: {
-  //       api_key: tmdbApiKey,
-  //     },
-  //     type: 'GET',
-  //     dataType: 'json',
-  //   })
-  //     .done(function(response) {
-  //       console.log('done', response);
-
-  //       for (var i = 0; i < response.results.length; i++) {
-  //         var movie = response.results[i];
-  //         var markup = getPopulatedMovieCardMarkup(movie);
-  //         $('.movie-cards-container').append(markup);
-  //       }
-
-  //     })
-  //     .fail(function(jqXHR, textStatus, errorThrown) {
-  //       console.log('fail', jqXHR);
-  //     })
-  //   ;
-  // });
+  // Document ready.
+  $(function() {
+    $.ajax({
+      url: tmdbBaseUrl + 'movie/popular',
+      data: {
+        api_key: tmdbApiKey,
+      },
+      type: 'GET',
+      dataType: 'json',
+    })
+      .done(function(response) {
+        for (var i = 0; i < response.results.length; i++) {
+          var movie = response.results[i];
+          var markup = getPopulatedMovieCardMarkup(movie);
+          $('.movie-cards-container').append(markup);
+        }
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('fail', jqXHR);
+        alert('Whoops, something failed.');
+      })
+    ;
+  });
 
 })();
